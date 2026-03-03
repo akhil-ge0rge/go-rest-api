1e6a9390b5bc78f2ef1a1fa660817637a51c4a45
Akhil George <akhil8311@gmail.com>
2026-03-03 16:21:17 +0530

    get single event by id

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"go-rest/db"
 	"go-rest/models"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -12,6 +13,7 @@ func main() {
 	db.InitDB()
 	server := gin.Default()
 	server.GET("/events", getEvents)
+	server.GET("/events/:id", getEvent)
 	server.POST("/events", createEvents)
 	server.Run(":8080") //localhost:8080
 }
@@ -27,6 +29,33 @@ func getEvents(context *gin.Context) {
 		return
 	}
 	context.JSON(http.StatusOK, gin.H{"message": "Sucess", "status_code": http.StatusOK, "data": events})
+}
+
+func getEvent(context *gin.Context) {
+	id, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"message":     "Failed",
+			"status_code": http.StatusBadRequest,
+			"error":       err.Error(),
+		})
+		return
+	}
+	event, err := models.GetSingleEvent(id)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{
+			"message":     "Failed",
+			"status_code": http.StatusInternalServerError,
+			"error":       err.Error(),
+		})
+		return
+	}
+	context.JSON(http.StatusOK, gin.H{
+		"message":     "Sucess",
+		"status_code": http.StatusOK,
+		"data":        event,
+	})
+
 }
 
 func createEvents(context *gin.Context) {
